1c3782a85b296f7d4d2b99143fa37592db725327
Noah <noah.beamon648@gmail.com>
2021-08-16 14:48:54 -0400

    deployment to production

---
+++ build/precache-manifest.98d330a3c39b8e394c406250fac59ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45868b740a6a62207821130dfe8096cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f46c96484ce8215524",
+    "url": "/static/css/main.0043149f.chunk.css"
+  },
+  {
+    "revision": "7c23a4b7905711b9944e",
+    "url": "/static/js/2.8b18bc4f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8b18bc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f46c96484ce8215524",
+    "url": "/static/js/main.0e2d1989.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
